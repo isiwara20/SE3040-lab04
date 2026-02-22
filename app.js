5c2621e0b5c7a12f2795a557fac0c2b1107be645
isiwara20 <oslocctv6@gmail.com>
2026-02-22 22:00:01 +0530

    Using Module
    Using Module

--- app.js
+++ app.js
@@ -1,24 +1,2 @@
-const condition = true;
-
-const myPromise = new Promise((resolve, reject) => {
-  if (condition) {
-    resolve('Async Success!');
-  } else {
-    reject('Async Failure!');
-  }
-});
-
-async function myFunction() {
-  try {
-    const result = await myPromise;
-    console.log(result);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-myFunction();
-console.log("Server running on http://localhost:8080");
 const myModule = require('./my-module.js');
-
 console.log(myModule.myFunction());
