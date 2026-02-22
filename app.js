5a20b4f17c1903599e868b8910a890faf0222fa2
isiwara20 <oslocctv6@gmail.com>
2026-02-22 21:51:09 +0530

    Add read file feature

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const http = require('http');
+const fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
-    res.end();
-}).listen(8080);
-
-console.log("Server running on http://localhost:8080");
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
