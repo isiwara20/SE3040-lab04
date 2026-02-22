74b053a29cd2a696808448b6f19b7613d453484a
isiwara20 <oslocctv6@gmail.com>
2026-02-22 21:57:59 +0530

    added websever
    added webserver

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
-    res.end();
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
 }).listen(8080);
-
-console.log("Server running on http://localhost:8080");
